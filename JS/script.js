aef671f3451d0c8e68f09546ccc0d2f1c82c01dc
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-07 14:17:58 +0200

    feat inserimento della variabile di controllo

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,4 @@
 const emailslist = ['gennaro@gmail.com', 'maria@gmail.com', 'raimondo@gmail.com', 'patrizia@gmail.com', 'biagio@gmail.com']
+const usermail = prompt ('Inserisci la tua email')
+
+let check = false
