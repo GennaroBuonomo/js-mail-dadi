08c4dc1abb5e843640031ad3e499661834d8d706
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-07 14:20:25 +0200

    feat inizio ciclo array

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,9 @@ const emailslist = ['gennaro@gmail.com', 'maria@gmail.com', 'raimondo@gmail.com'
 const usermail = prompt ('Inserisci la tua email')
 
 let check = false
+
+for(let i=0; i<emailslist.length; i++){
+  if(usermail === emailslist[i]){
+    check = true
+  }
+}
